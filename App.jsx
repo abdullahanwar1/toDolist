78fb06c4862a78c3d1637045e3014c010a1d157a
abdullahanwar1 <120066719+abdullahanwar1@users.noreply.github.com>
2023-11-17 18:13:30 -0700

    lab4

--- App.jsx
+++ App.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native';
 import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
-
-function App() { <SafeAreaView>
-  <ToDoList />
 function App() {  const [tasks, setTasks] = useState([
   'Do laundry',
   'Go to gym',
   'Walk dog'
 ]);
+// Function to add a new task
+const addTask = (taskText) => {
+  // Prevent adding an empty task or a duplicate task
+  if (taskText && !tasks.includes(taskText)) {
+    setTasks([...tasks, taskText]);
+  }
+};
 <SafeAreaView>
 <ToDoList tasks={tasks} />
   <ToDoForm />
+<ToDoForm addTask={addTask} />
 </SafeAreaView>
 ;
+}
+export default App;
--- ToDoForm.jsx
+++ ToDoForm.jsx
@@ -1,33 +1,31 @@
 // ToDoForm.jsx
 
 import React from 'react';
+import React, {useState} from 'react';
 import {View, TextInput, Button, StyleSheet} from 'react-native';
 
 function ToDoForm() {
+function ToDoForm({addTask}) {
+    const [taskText, setTaskText] = useState('');
+
+    const handleSubmit = () => {
+      addTask(taskText);
+      setTaskText(''); // Clear the input field after adding the task
+    };
+
     return (
         <View style={styles.form}>
-      <TextInput style={styles.input} placeholder="Add a new task..." />
-      <Button title="Add" />
+        <TextInput style={styles.input} placeholder="Add a new task..." />
+        <Button title="Add" />
         </View>
+        <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a new task..."
+          onChangeText={setTaskText}
+          value={taskText}
+        />
+        <Button title="Add" onPress={handleSubmit} />
+      </View>
     );
-    }
-
-const styles = StyleSheet.create({
-    form: {
-        flexDirection: 'row',
-    justifyContent: 'space-between',
-        alignItems: 'center',
-        marginHorizontal: 20,
-        marginTop: 20,
-    },
-    input: {
-        flex: 1,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        paddingHorizontal: 10,
-        paddingVertical: 5,
-        marginRight: 10,
-    },
-});
-
-export default ToDoForm;
+}
