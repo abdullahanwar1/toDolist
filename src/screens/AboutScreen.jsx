35c372eb2ec5679b5c7214b23b72f66b7e657f81
abdullahanwar1 <120066719+abdullahanwar1@users.noreply.github.com>
2023-11-24 21:56:24 -0700

    lab 5

--- src/screens/AboutScreen.jsx
+++ src/screens/AboutScreen.jsx
@@ -1,89 +1,33 @@
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  Text,
-  Button,
-  StyleSheet,
-  TouchableOpacity,
-  Modal,
-  View,
-} from 'react-native';
-import MainLayout from '../layouts/MainLayout';
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import MainLayout from '../layouts/MainLayout.jsx';
+import {Button} from 'react-native';
+import {TouchableOpacity} from 'react-native';
 
 function AboutScreen({navigation}) {
-  const [currentDate, setCurrentDate] = useState(
-    new Date().toLocaleDateString(),
-  );
-  const [showEasterEgg, setShowEasterEgg] = useState(false);
+  const appName = 'My To Do List App';
+  const authorName = 'Max Hui'; // Replace with your name
+  const currentDate = new Date().toDateString();
 
-  const handleNamePress = () => {
-
-    setShowEasterEgg(true);
-  };
-
-  const closeEasterEgg = () => {
-
-    setShowEasterEgg(false);
+  const handleAuthorNamePress = () => {
+    alert(`Thanks for using this app, from ${authorName}`);
   };
 
   return (
     <MainLayout>
       <SafeAreaView>
-        <Text style={styles.title}>About tolistapp</Text>
-        <TouchableOpacity onPress={handleNamePress}>
+        <Text>{appName}</Text>
+        <TouchableOpacity onPress={handleAuthorNamePress}>
+          <Text>Author: {authorName}</Text>
         </TouchableOpacity>
-        <Text style={styles.date}>Current Date: {currentDate}</Text>
+        <Text>Current Date: {currentDate}</Text>
         <Button
-          title="Go back to Home"
+          title="Go to Home"
           onPress={() => navigation.navigate('Home')}
         />
       </SafeAreaView>
-
-
-      <Modal visible={showEasterEgg} animationType="slide" transparent={true}>
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <Text style={styles.modalText}>You found the Easter egg!</Text>
-            <Button title="Close" onPress={closeEasterEgg} />
-          </View>
-        </View>
-      </Modal>
     </MainLayout>
   );
 }
-
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  author: {
-    fontSize: 18,
-    marginBottom: 10,
-    textDecorationLine: 'none', 
-  },
-  date: {
-    fontSize: 16,
-    color: '#555',
-    marginBottom: 20,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    padding: 20,
-    borderRadius: 10,
-    elevation: 5, 
-  },
-  modalText: {
-    fontSize: 18,
-    marginBottom: 10,
-  },
-});
 
 export default AboutScreen;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,16 +1,15 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Button} from 'react-native';
-import ToDoList from '../components/ToDoList';
-import ToDoForm from '../components/ToDoForm';
-import MainLayout from '../layouts/MainLayout';
+import {SafeAreaView} from 'react-native';
+import ToDoList from '../components/ToDoList.jsx';
+import ToDoForm from '../components/ToDoForm.jsx';
+import MainLayout from '../layouts/MainLayout.jsx';
+import {Button} from 'react-native';
 
 function HomeScreen({navigation}) {
   const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
 
-  const addTask = taskText => {
-    if (taskText && !tasks.includes(taskText)) {
-      setTasks([...tasks, taskText]);
-    }
+  const addTask = task => {
+    if (!tasks.includes(task)) setTasks([...tasks, task]);
   };
 
   return (
